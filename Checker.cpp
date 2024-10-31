d67d5eb6e50ba87bd7e203d43ab6f5b18a60d57c
poojitadevi <60169987+poojitadevi@users.noreply.github.com>
2024-10-31 13:19:27 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -1,29 +1,71 @@
 #include <assert.h>
+#include <cassert>
 #include <iostream>
-using namespace std;
 
-bool checkRange(float value, float min, float max, const string& message) {
-  if (value < min || value > max) {
-    cout << message << endl;
+void EarlyWarning(float parameter, float minLimit , float maxLimit)
+{
+    float ToleRance = parameter * 0.05 ;
+    float minTolerance = parameter + ToleRance;
+    float maxTolerance = parameter - ToleRance;
+
+  if(parameter >= minLimit && parameter <= minTolerance)
+    std::cout<<"Warning: Approaching discharge \n";
+  else if (parameter >= maxTolerance && parameter <= maxLimit)
+    std::cout<<"Warning: Approaching charge-peak \n";
+}
+using namespace std;
+uint8_t batterycheck(bool temperatureCheck, bool socCheck, bool chargeRateCheck) {
+  return temperatureCheck&&socCheck&&chargeRateCheck;
+}
+bool temperaturecheck(float temperature)
+{  
+    if(temperature < 0 || temperature > 45) {
+    cout << "Temperature out of range!\n";
+    return false;
+    }
+    else
+    {
+      EarlyWarning(temperature,0,45);
+      return true;
+    }
+   
+}
+bool soccheck(float soc)
+{
+  if(soc < 20 || soc > 80) {
+    cout << "State of Charge out of range!\n";
     return false;
   }
-  return true;
+  else
+  {
+    EarlyWarning(soc,20,80);
+    return true;
+  }
 }
-
+bool chargeratecheck(float chargeRate)
+{
+  if(chargeRate > 0.8) {
+    cout << "Charge Rate out of range!\n";
+    return false;
+  }
+  else
+  {
+    EarlyWarning(chargeRate,0,0.8);
+    return true;
+  }
+}
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  bool isTemperatureOk = checkRange(temperature, 0, 45, "Temperature out of range!");
-  bool isSocOk = checkRange(soc, 20, 80, "State of Charge out of range!");
-  bool isChargeRateOk = checkRange(chargeRate, 0, 0.8, "Charge Rate out of range!");
-
-  return isTemperatureOk && isSocOk && isChargeRateOk;
+   
+bool tempflag    = temperaturecheck(temperature);
+bool socflag     = soccheck(soc);
+bool chargeflag  = chargeratecheck(chargeRate); 
+        
+  return batterycheck(tempflag, socflag, chargeflag) ;
 }
-
 
 int main() {
+  
   assert(batteryIsOk(25, 70, 0.7) == true);
   assert(batteryIsOk(50, 85, 0) == false);
-  assert(batteryIsOk(-1, 60, 0.3) == false);
-  assert(batteryIsOk(35, 82, 0.3) == false);
-  assert(batteryIsOk(42, 40, 0.9) == false);
-  
+  return 0;
 }
