d7ff3b3229b5a9cba65c3e84d9c56ba6cd0ae218
poojitadevi <60169987+poojitadevi@users.noreply.github.com>
2024-10-31 13:25:01 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -1,71 +1,35 @@
-#include <assert.h>
-#include <cassert>
 #include <iostream>
+#include <cassert>
 
-void EarlyWarning(float parameter, float minLimit , float maxLimit)
-{
-    float ToleRance = parameter * 0.05 ;
-    float minTolerance = parameter + ToleRance;
-    float maxTolerance = parameter - ToleRance;
+void EarlyWarning(float parameter, float minLimit, float maxLimit) {
+    float tolerance = parameter * 0.05;
 
-  if(parameter >= minLimit && parameter <= minTolerance)
-    std::cout<<"Warning: Approaching discharge \n";
-  else if (parameter >= maxTolerance && parameter <= maxLimit)
-    std::cout<<"Warning: Approaching charge-peak \n";
-}
-using namespace std;
-uint8_t batterycheck(bool temperatureCheck, bool socCheck, bool chargeRateCheck) {
-  return temperatureCheck&&socCheck&&chargeRateCheck;
-}
-bool temperaturecheck(float temperature)
-{  
-    if(temperature < 0 || temperature > 45) {
-    cout << "Temperature out of range!\n";
-    return false;
+    if (parameter <= minLimit + tolerance) {
+        std::cout << "Warning: Approaching discharge\n";
+    } else if (parameter >= maxLimit - tolerance) {
+        std::cout << "Warning: Approaching charge-peak\n";
     }
-    else
-    {
-      EarlyWarning(temperature,0,45);
-      return true;
+}
+
+bool checkInRange(float parameter, float minLimit, float maxLimit, const std::string& warningMessage) {
+    if (parameter < minLimit || parameter > maxLimit) {
+        std::cout << warningMessage << " out of range!\n";
+        return false;
     }
-   
-}
-bool soccheck(float soc)
-{
-  if(soc < 20 || soc > 80) {
-    cout << "State of Charge out of range!\n";
-    return false;
-  }
-  else
-  {
-    EarlyWarning(soc,20,80);
+    EarlyWarning(parameter, minLimit, maxLimit);
     return true;
-  }
 }
-bool chargeratecheck(float chargeRate)
-{
-  if(chargeRate > 0.8) {
-    cout << "Charge Rate out of range!\n";
-    return false;
-  }
-  else
-  {
-    EarlyWarning(chargeRate,0,0.8);
-    return true;
-  }
-}
+
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
-   
-bool tempflag    = temperaturecheck(temperature);
-bool socflag     = soccheck(soc);
-bool chargeflag  = chargeratecheck(chargeRate); 
-        
-  return batterycheck(tempflag, socflag, chargeflag) ;
+    bool tempOk = checkInRange(temperature, 0, 45, "Temperature");
+    bool socOk = checkInRange(soc, 20, 80, "State of Charge");
+    bool chargeRateOk = checkInRange(chargeRate, 0, 0.8, "Charge Rate");
+
+    return tempOk && socOk && chargeRateOk;
 }
 
 int main() {
-  
-  assert(batteryIsOk(25, 70, 0.7) == true);
-  assert(batteryIsOk(50, 85, 0) == false);
-  return 0;
+    assert(batteryIsOk(25, 70, 0.7) == true);
+    assert(batteryIsOk(50, 85, 0) == false);
+    return 0;
 }
